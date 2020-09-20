ba4a08def57a4217ac1c0d2579d250f734d9790c
Blake Williams <blakethewilliams@gmail.com>
2020-09-19 20:48:42 -0700

    Update app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import App from 'next/app'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import Head from 'next/head'
 
 library.add(fas)
 
@@ -8,7 +9,12 @@ export default class MyApp extends App {
   render () {
     const { Component, pageProps } = this.props
     return (
-      <Component { ...pageProps } />
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        </Head>
+        <Component { ...pageProps } />
+      </>
     )
   }
 }
